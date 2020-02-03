892354e774be64e6fbd7431db3060e84485a5218
AshCott <Ashley.Cottrell@outlook.com>
2020-02-03 20:44:18 +1000

    Updates

---
+++ precache-manifest.6dd9f48842b950c940ed73ef6bcb59c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d7ec57b445e52ccf1d29101a4faa1d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "55babe4e6a9336e40607",
+    "url": "/react-portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "c14aca4a7ee668dd2f2e",
+    "url": "/react-portfolio/static/css/main.b52e69e7.chunk.css"
+  },
+  {
+    "revision": "55babe4e6a9336e40607",
+    "url": "/react-portfolio/static/js/2.9de25068.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/react-portfolio/static/js/2.9de25068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14aca4a7ee668dd2f2e",
+    "url": "/react-portfolio/static/js/main.c3837903.chunk.js"
+  },
+  {
+    "revision": "0bf244b9126ae1bcf469",
+    "url": "/react-portfolio/static/js/runtime-main.22f696ad.js"
+  }
+]);
